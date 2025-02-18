0a39e59df22ba124ee6cc8acddbfb8a3463f835b
kimsohea <kimsohea4425@gmail.com>
2025-02-18 15:17:07 +0900

    script: xmlHttpRequest 코드 위치 변경

--- script.js
+++ script.js
@@ -14,28 +14,11 @@ const month =
 const date =
   todayDate[2] < 10 ? "&solDay=0" + todayDate[2] : "&solDay=" + todayDate[2];
 const requestUrl = dataApiUrl + dataApiKey + year + month + date;
-const respon = new XMLHttpRequest();
-respon.open("GET", requestUrl);
-respon.send();
 
-respon.onload = function () {
-  if (respon.status >= 200 && respon.status < 300) {
-    let parseXML = new DOMParser();
-    let xmlDoc = parseXML.parseFromString(respon.response, "text/xml");
-    let todayMoon = xmlDoc.querySelector("body items item lunDay").textContent;
-    if (todayMoon < 2) todayMoonIdx = 0;
-    else if (todayMoon > 2 && todayMoon < 8) todayMoonIdx = 1;
-    else if (todayMoon >= 8 && todayMoon < 9) todayMoonIdx = 2;
-    else if (todayMoon > 9 && todayMoon < 15) todayMoonIdx = 3;
-    else if (todayMoon === 15) todayMoonIdx = 4;
-    else if (todayMoon > 15 && todayMoon < 22) todayMoonIdx = 5;
-    else if (todayMoon >= 22 && todayMoon < 24) todayMoonIdx = 6;
-    else todayMoonIdx = 7;
-  } else {
-    console.log("failed");
-  }
-};
 document.addEventListener("DOMContentLoaded", () => {
+  const respon = new XMLHttpRequest();
+  respon.open("GET", requestUrl);
+  respon.send();
   respon.onload = function () {
     if (respon.status >= 200 && respon.status < 300) {
       let parseXML = new DOMParser();
